f297d5fa500e5c449a102c6f742a52d933a683f4
Lorg0n <lorgon.kv@gmail.com>
2026-06-15 15:33:51 +0300

    chore: bump ro 0.2 version

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   manifest: {
     name: "MangaFit - Comic & Webtoon Resizer",
     description: "Easily scale and fit images or comic panels on any website.",
-    version: "0.1",
+    version: "0.2",
     permissions: ["storage", "activeTab", "scripting"],
     host_permissions: ["<all_urls>"]
   }
