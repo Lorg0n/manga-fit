87af663c35bef6ea85d94f9a6d4fe156019ff51f
Lorg0n <lorgon.kv@gmail.com>
2026-06-19 03:26:17 +0300

    fix: firefox manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    name: "MangaFit - Comic & Webtoon Resizer",
-    description: "Easily scale and fit images or comic panels on any website.",
-    version: "0.2",
-    permissions: ["storage", "activeTab", "scripting"],
-    host_permissions: ["<all_urls>"]
+    name: 'MangaFit - Comic & Webtoon Resizer',
+    description: 'Easily scale and fit images or comic panels on any website.',
+    version: '0.2',
+
+    permissions: ['storage', 'activeTab', 'scripting'],
+    host_permissions: ['<all_urls>'],
+
+    browser_specific_settings: {
+      gecko: {
+        data_collection_permissions: {
+          required: ['none']
+        }
+      }
+    }
   }
 });
